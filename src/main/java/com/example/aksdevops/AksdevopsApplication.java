b1277019bc21ae24d9fbadd3be2d61db3b5239b6
Akshay Nair <akshayn@druva.local>
2023-06-13 00:50:49 +0530

    CodePush

---
+++ src/main/java/com/example/aksdevops/AksdevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.aksdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AksdevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AksdevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aksdevops/AksdevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aksdevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AksdevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
