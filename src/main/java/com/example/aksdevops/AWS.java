24b582e05df42ab882b6a0cc62500b257d5ff42c
Akshay Nair <akshayn@druva.local>
2023-06-16 03:56:21 +0530

    NewClas

---
+++ src/main/java/com/example/aksdevops/AWS.java
@@ -0,0 +1,9 @@
+package com.example.aksdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+public class AWS {
+    public String getData()
+    { return "We are learning AWS";
+    }
+}
